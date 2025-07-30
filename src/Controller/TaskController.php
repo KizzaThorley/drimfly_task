497f2cd945ab8fd73d451c74ab4ef20ba56bac60
kizza <kieran.thorley@gmail.com>
2025-07-30 20:50:23 +0100

    -working inital crud functionality -added changes to html boilpate to properly loop and then have line through and x for active

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,78 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
-    #[Route('/tasks')]
-    public function home() : Response
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-     return $this->render('tasks/homepage.html.twig');
+        $this->em = $em;
     }
 
+    #[Route('/tasks', name: 'task_list', methods: ['GET'])]
+    public function list(): Response
+    {
+        $tasks = $this->em->getRepository(Task::class)->findBy(['deletedAt' => null]);
+
+        return $this->render('tasks/homepage.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/tasks', name: 'task_add', methods: ['POST'])]
+    public function add(Request $request): RedirectResponse
+    {
+        $title = $request->request->get('title');
+        if (!$title) {
+            return $this->redirectToRoute('task_list');
+        }
+    
+        $task = new Task();
+        $task->setTitle($title);
+        $task->setIsDone(false);
+    
+        $this->em->persist($task);
+        $this->em->flush();
+    
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route('/tasks/{id}/toggle', name: 'task_toggle', methods: ['POST'])]
+    public function toggle(int $id): RedirectResponse
+    {
+        $task = $this->em->getRepository(Task::class)->find($id);
+
+        if (!$task || $task->getDeletedAt() !== null) {
+            $this->addFlash('error', 'Task not found');
+            return $this->redirectToRoute('task_list');
+        }
+
+        $task->setIsDone(!$task->getIsDone());
+        $this->em->flush();
+
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route('/tasks/{id}/delete', name: 'task_delete', methods: ['POST'])]
+    public function delete(int $id): RedirectResponse
+    {
+        $task = $this->em->getRepository(Task::class)->find($id);
+
+        if (!$task || $task->getDeletedAt() !== null) {
+            $this->addFlash('error', 'Task not found');
+            return $this->redirectToRoute('task_list');
+        }
+
+        $task->setDeletedAt(new \DateTime());
+        $this->em->flush();
+
+        return $this->redirectToRoute('task_list');
+    }
 }
---
+++ src/Entity/Task.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Table(name: 'tasks')]
+#[ORM\HasLifecycleCallbacks]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private string $title;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private bool $isDone = false;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $deletedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIsDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): self
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeInterface
+    {
+        return $this->deletedAt;
+    }
+
+    public function setDeletedAt(?\DateTimeInterface $deletedAt): self
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function onPrePersist(): void
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+}
